a5aae135bad09cd2b2440053abd8bc96b6102eee
thiago-anjos <thiago.w3c@gmail.com>
2020-02-23 19:45:18 -0300

    Updates

---
+++ precache-manifest.823ca7c5c8e69dd255b13e589aadaab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c753453858fc0e270e96465eb02b3d",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "77778b798ae1e24f8599",
+    "url": "/react-basics/static/css/main.d50a57a6.chunk.css"
+  },
+  {
+    "revision": "da42da225f9363d9a545",
+    "url": "/react-basics/static/js/2.a855421b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-basics/static/js/2.a855421b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77778b798ae1e24f8599",
+    "url": "/react-basics/static/js/main.a661f075.chunk.js"
+  },
+  {
+    "revision": "c41efa39de0f8021201c",
+    "url": "/react-basics/static/js/runtime-main.6a1ea095.js"
+  }
+]);
